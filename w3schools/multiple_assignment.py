c35f79b498a89b6aced041aa7ab0f11c863157e1
Shahryar Salmaan <shahryarsalmaan@gmail.com>
2025-06-30 23:17:37 +0000

    Python Variables - Assign Multiple Values

---
+++ w3schools/multiple_assignment.py
@@ -0,0 +1,15 @@
+# Assigning multiple values to variables
+
+a, b, c = "black", "white", "grey"
+
+print(a)
+print(b)
+print(c)
+
+# Assigning one value to multiple variables
+
+x = y = z = "yellow"
+
+print(x)
+print(y)
+print(z)
