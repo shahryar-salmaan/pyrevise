ba38520592e5a6303c7ac8cb648fe927c4ab7636
Shahryar Salmaan <shahryarsalmaan@gmail.com>
2025-07-01 00:22:59 +0000

    Slicing Strings

---
+++ w3schools/slicing_strings.py
@@ -0,0 +1,11 @@
+# Slicing Strings
+
+b = "ONE-TWO-THREE"
+
+print(b[:3]) # ONE
+print(b[4:7]) # TWO
+print(b[8:]) # THREE
+
+print(b[:-10]) # ONE (REVERSE)
+print(b[-9:-6]) # TWO (REVERSE)
+print(b[-5:]) # THREE (REVERSE)
