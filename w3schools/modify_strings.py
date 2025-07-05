2aac3322256e33fb80aca780207c8f23f60e62cc
Shahryar Salmaan <shahryarsalmaan@gmail.com>
2025-07-05 17:57:17 +0000

    Modify Strings

---
+++ w3schools/modify_strings.py
@@ -0,0 +1,15 @@
+# Modify Strings
+
+a: str = "Sky is blue."
+
+print(a.upper()) # Uppercase
+print(a.lower()) # Lowercase
+
+b: str = "  Green looks good on you!  "
+print(b.strip()) # Remove whitespaces
+
+c: str = "Yellow, Cyan, Pink"
+print(c.replace("Pink", "Blue")) # Replace
+
+d: str = "Tic-Tac-Toe"
+print(d.split("-")) # Split
